b98d3c09a696aa5bef9a4d75282f4bfdb97ebc1b
Ryan Lee <drdgvhbh@gmail.com>
2020-08-11 10:57:39 -0400

    Elanora Vo Damos

--- backend/src/entities/feed-item.ts
+++ backend/src/entities/feed-item.ts
@@ -1,16 +1,19 @@
-import { withValidate } from 'io-ts-types'
+import { withValidate, withMessage } from 'io-ts-types'
 import { summonFor } from '@morphic-ts/batteries/lib/summoner-ESBASTJ'
-import { ObjectID } from '../mongodb/ObjectID'
-
-/* const FeedItem = S.make((F) =>
-  F.type({
-    _id: F.ObjectID,
-    title: F.string,
-  }),
-) */
+import {
+  ObjectIDFromObjectIDProto,
+  ObjectIDSummoner,
+} from '../mongodb/ObjectID'
+import * as E from 'fp-ts/lib/Either'
+import * as mongodb from 'mongodb'
+import { flow, pipe } from 'fp-ts/lib/function'
+import util from 'util'
+import { formatValidationErrors } from 'io-ts-reporters'
 
 interface IoTsTypes {
   withValidate: typeof withValidate
+  withMessage: typeof withMessage
+  ObjectIDFromString: typeof ObjectIDFromObjectIDProto
 }
 
 type SummonConfig = {
@@ -18,15 +21,34 @@ type SummonConfig = {
 }
 
 const { summon } = summonFor<SummonConfig>({
-  IoTsURI: { withValidate },
+  IoTsURI: {
+    withValidate,
+    withMessage,
+    ObjectIDFromString: ObjectIDFromObjectIDProto,
+  },
 })
 
 const FeedItem = summon((F) =>
   F.interface(
     {
-      _id: F.string({ IoTsURI: (codec, env) => env.withValidate(ObjectID) }),
+      _id: ObjectIDSummoner(F),
       title: F.string(),
     },
     'FeedItem',
+  ),
+)
+
+pipe(
+  FeedItem.type.decode({
+    _id: '5f31f22918b5a6853a74912d',
+    title: 'asdf',
+  }),
+  E.fold(
+    flow(
+      formatValidationErrors,
+      (a) => util.inspect(a, { depth: null }),
+      console.log,
+    ),
+    flow((a) => util.inspect(a), console.log),
   ),
 )
--- backend/src/mongodb/ObjectID.ts
+++ backend/src/mongodb/ObjectID.ts
@@ -1,29 +1,66 @@
-import * as C from 'io-ts/lib/Codec'
-import * as D from 'io-ts/lib/Decoder'
-import * as E from 'io-ts/lib/Encoder'
-import { pipe } from 'fp-ts/lib/function'
+import * as O from 'fp-ts/lib/Option'
 import * as mongodb from 'mongodb'
+import * as t from 'io-ts'
+
+import { withValidate, withMessage } from 'io-ts-types'
+import { summonFor } from '@morphic-ts/batteries/lib/summoner-ESBASTJ'
+import { pipe } from 'fp-ts/lib/function'
 
 export interface ObjectIDBrand {
   readonly ObjectID: unique symbol
 }
 
-export type ObjectID = string & ObjectIDBrand
-
-export const decoder = pipe(
-  D.fromRefinement(
-    (u): u is mongodb.ObjectID => u instanceof mongodb.ObjectID,
-    'ObjectID',
-  ),
-  D.map((id) => id.toHexString()),
-  D.refine((id): id is ObjectID => !!id, 'ObjectID'),
+export const ObjectID = t.brand(
+  t.string,
+  (s): s is t.Branded<string, ObjectIDBrand> =>
+    O.isSome(O.tryCatch(() => new mongodb.ObjectID(s))),
+  'ObjectID',
 )
 
-const encoder: E.Encoder<mongodb.ObjectID, ObjectID> = {
-  encode: (a) => new mongodb.ObjectID(a),
+export type ObjectID = t.TypeOf<typeof ObjectID>
+
+export const ObjectIDFromObjectIDProto = new t.Type<
+  ObjectID,
+  mongodb.ObjectID,
+  unknown
+>(
+  'ObjectIDFromObjectIDProto',
+  ObjectID.is,
+  (u, c) =>
+    u instanceof mongodb.ObjectID
+      ? ObjectID.asDecoder().validate(u.toHexString(), c)
+      : t.failure(u, c),
+  (a) => new mongodb.ObjectID(a),
+)
+
+// -- morphic-ts
+
+interface IoTsTypes {
+  withValidate: typeof withValidate
+  withMessage: typeof withMessage
+  ObjectIDFromString: typeof ObjectIDFromObjectIDProto
 }
 
-export const ObjectID: C.Codec<unknown, mongodb.ObjectID, ObjectID> = C.make(
-  decoder,
-  encoder,
+type SummonConfig = {
+  IoTsURI: IoTsTypes
+}
+
+const { summon } = summonFor<SummonConfig>({
+  IoTsURI: {
+    withValidate,
+    withMessage,
+    ObjectIDFromString: ObjectIDFromObjectIDProto,
+  },
+})
+
+export const ObjectIDSummoner = summon((F) =>
+  F.refined(F.string(), ObjectID.is, ObjectID.name, {
+    IoTsURI: (_codec, env) =>
+      pipe(env.withValidate(ObjectID, env.ObjectIDFromString.decode), (c) =>
+        env.withMessage(
+          c,
+          (x) => `expected \"${x}\" to be an instance of mongodb.ObjectID`,
+        ),
+      ),
+  }),
 )
